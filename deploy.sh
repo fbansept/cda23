7d86e592a5119692efe53a1caf0c7ec60e69fbf1
fbansept <bansept.franck@gmail.com>
2023-06-12 14:19:44 +0200

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Mettre à jour le code source
+git pull
+
+# Construire l'image Docker
+docker build --no-cache -t angular-demo-franck .
+
+# Arreter le conteneur existant
+docker stop angular-demo-franck
+
+# Supprimer le conteneur existant
+docker rm angular-demo-franck
+
+# Lancer un nouveau conteneur
+docker run -d --net backend --ip 172.18.0.6 --name=angular-demo-franck -p 80:8080
